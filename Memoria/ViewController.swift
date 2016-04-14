e07f4c406078aedb4f595d698ba39e97825a73a8
José Alves <jose.tapadas@gmail.com>
2016-04-14 18:04:41 +0100

    initial commit

--- Memoria/ViewController.swift
+++ Memoria/ViewController.swift
@@ -12,14 +12,69 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view, typically from a nib.
+        setupButtons()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func setupButtons() {
+        view.backgroundColor = UIColor.whiteColor()
+        
+        buildBUttonWithCenter(CGPoint(x: view.center.x, y: view.center.y / 2.0), title: "Apprentice", color: UIColor.greenColor(), action: #selector(ViewController.onApprenticeClick))
+        
+        buildBUttonWithCenter(CGPoint(x: view.center.x, y: view.center.y), title: "Regular", color: UIColor.yellowColor(), action: #selector(ViewController.onRegularClick))
+        
+        buildBUttonWithCenter(CGPoint(x: view.center.x, y: view.center.y + view.center.y / 2.0), title: "Master", color: UIColor.redColor(), action: #selector(ViewController.onMasterClick))
+        
+    }
 }
 
+
+// Creating Buttons
+private extension ViewController {
+    func buildBUttonWithCenter(center: CGPoint, title: String, color: UIColor, action: Selector) {
+        let button = UIButton()
+        
+        button.setTitle(title, forState: .Normal)
+        button.frame = CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 200, height: 50))
+        button.center = center
+        button.backgroundColor = color
+        button.addTarget(self, action: action, forControlEvents: UIControlEvents.TouchUpInside)
+        
+        view.addSubview(button)
+    }
+    
+    @objc func onApprenticeClick(sender: UIButton) {
+        setNewGameLevel(.Apprentice)
+    }
+    
+    @objc func onRegularClick(sender: UIButton) {
+        setNewGameLevel(.Regular);
+    }
+    
+    @objc func onMasterClick(sender: UIButton) {
+        setNewGameLevel(.Master);
+    }
+}
+
+private extension ViewController {
+    enum Level {
+        case Apprentice, Regular, Master
+    }
+    
+    func setNewGameLevel(level: Level) {
+        switch level {
+        case .Apprentice:
+            NSLog("Apprentice");
+        case .Regular:
+            NSLog("Regular!")
+        case .Master:
+            NSLog("Master!")
+        }
+    }
+}
